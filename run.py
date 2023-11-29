74b701ad55eabc02f9af77561485f114358b8c9c
Marwan900 <marwan.elfakih@gmail.com>
2023-11-29 16:24:03 +0100

    test

--- run.py
+++ run.py
@@ -7,12 +7,29 @@ def main():
     """
     Main
     """
-    import_data()
+    data = import_data()
+    data = rename_columns(data)
 
-def import_data():
-    data=pd.read_csv("data/iris.csv")
+    
+def import_data() -> pd.DataFrame:
+    """
+    Import csv file as a dataframe
+    Output: data [pd.DataFrame]
+    """
+    data = pd.read_csv("data/iris.csv")
     print(data.shape)
     return data
+
+def rename_columns(data: pd.DataFrame) -> pd.DataFrame:
+    """
+    Doc
+    """
+    data_renamed = data.rename(columns={"sepal.length": 'sepal_length',
+                                "sepal.width": 'sepal_width',
+                                "petal.length": 'petal_length',
+                                "petal.width": 'petal_width'})
+    
+    return data_renamed
 
 
 if __name__ == '__main__':
--- test_run.py
+++ test_run.py
@@ -1,11 +1,20 @@
 """
 Tests
 """
-from run import import_data
+from run import import_data, rename_columns
 
 def test_import_data():
     """
     Test
     """
     data = import_data()
-    assert data.shape[0]>0
+    assert data.shape[0] > 0
+
+
+def test_rename_columns():
+    """
+    Doc
+    """
+    data = import_data()
+    data_renamed = rename_columns(data)
+    assert "sepal_length" in data_renamed.columns
