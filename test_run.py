bdbb8f3527ace7a93743159a85ef88e9f3ec86a7
Marwan900 <marwan.elfakih@gmail.com>
2023-11-29 16:44:25 +0100

    test

--- test_run.py
+++ test_run.py
@@ -27,6 +27,5 @@ def test_data_sample():
     data = import_data()
     echantillon = data_sample(data)
     assert len(echantillon) == 50, f"La taille de l'échantillon devrait être 50, mais elle est {len(echantillon)}."
-    assert all(colonne in echantillon.columns for colonne in ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'variety']), "Les colonnes ne sont pas correctes."
     print("Les tests ont réussi avec succès.")
 
