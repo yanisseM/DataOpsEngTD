743a1c4050e13c839c7cfdf4fd3f2f1d9906d165
Marwan900 <marwan.elfakih@gmail.com>
2023-11-29 16:43:02 +0100

    fonction sample

--- test_run.py
+++ test_run.py
@@ -1,7 +1,7 @@
 """
 Tests
 """
-from run import import_data, rename_columns
+from run import import_data, rename_columns,data_sample
 
 def test_import_data():
     """
@@ -19,7 +19,6 @@ def test_rename_columns():
     data_renamed = rename_columns(data)
     assert "sepal_length" in data_renamed.columns
 
-import pandas as pd
 
 def test_data_sample():
     """
@@ -29,9 +28,5 @@ def test_data_sample():
     echantillon = data_sample(data)
     assert len(echantillon) == 50, f"La taille de l'échantillon devrait être 50, mais elle est {len(echantillon)}."
     assert all(colonne in echantillon.columns for colonne in ['sepal_length', 'sepal_width', 'petal_length', 'petal_width', 'variety']), "Les colonnes ne sont pas correctes."
-
     print("Les tests ont réussi avec succès.")
-
-# Appel de la fonction de test
-test_data_sample()
 
